315b3068220eaea2cc0d01c18fcfbacbddab1d71
Sandeep Reddy <msande1996@gmail.com>
2017-09-14 02:12:06 +0530

    Add repo checking module

---
+++ details.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+
+"""
+Returns tuple with stars,forks and watchers. 
+"""
+
+def get_details(repo_url):
+	try:
+		repo_details = requests.get(repo_url)
+		repo_details = repo_details.json()
+		return repo_details["stargazers_count"],repo_details["forks"],repo_details["subscribers_count"]
+	except ConnectionError as e:
+		return -1,-1,-1
